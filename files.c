7d0964fcd5f347ea7131d0f6b225bf30d08d0894
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-09-29 17:59:42 -0500

    Update files.c
    esta es la buena?

--- files.c
+++ files.c
@@ -13,9 +13,9 @@ void imprimircpu(int cont,double cpu_time, FILE * arch)
     fprintf(arch, "\n%d, %f", cont, cpu_time);
     
 }
-void imprimirres(double res[],FILE * arch)
+void imprimirres(int count, double res[],FILE * arch)
 {
     int i;
-    for(i=0;i<10;i++)
-    fprintf(arch, "\n%d, %f", i, res[i]);
+    for(i=0;i<count;i++)
+    fprintf(arch, "\n%d, %f", count, res[i]);
 }
