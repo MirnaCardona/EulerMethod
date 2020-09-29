6a2d8694b9d1dfbabf3507bf066bc7ccfc4d0b4b
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-09-29 17:50:04 -0500

    Update files.c

--- files.c
+++ files.c
@@ -13,7 +13,9 @@ void imprimircpu(int cont,double cpu_time, FILE * arch)
     fprintf(arch, "\n%d, %f", cont, cpu_time);
     
 }
-void imprimirres(int cont,double res,FILE * arch)
+void imprimirres(double res[],FILE * arch)
 {
-    fprintf(arch, "\n%d, %f", cont, res);
+    int i;
+    for(i=0;i<10;i++)
+    fprintf(arch, "\n%d, %f", i, res[i]);
 }
