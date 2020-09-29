7e870153bee4dce167c14834a4321a6fb23c4122
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-09-29 17:16:24 -0500

    Update files.c

--- files.c
+++ files.c
@@ -8,7 +8,12 @@
 #include "files.h"
 #include "main.h"
 
-void imprimir(int cont,double cpu_time,double res,FILE * arch1, FILE * arch2){
-    fprintf(arch1, "\n%d, %f", cont, cpu_time);
-    fprintf(arch2, "\n%d, %f", cont, res);
+void imprimircpu(int cont,double cpu_time, FILE * arch)
+{
+    fprintf(arch, "\n%d, %f", cont, cpu_time);
+    
+}
+void imprimirres(int cont,double res,FILE * arch)
+{
+    fprintf(arch, "\n%d, %f", cont, res);
 }
